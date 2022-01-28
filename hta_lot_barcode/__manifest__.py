23dec0c5c8bdd853bac1f7ac5bea0f1f49c03536
ATCHE Maurice <maurice.atche@halltech-africa.com>
2022-01-28 06:42:02 +0000

    Adding hta_lot_barcode

--- hta_lot_barcode/__manifest__.py
+++ hta_lot_barcode/__manifest__.py
@@ -17,10 +17,10 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.1',
+    'version': '15.0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['mrp', 'stock'],
 
     # always loaded
     'data': [
