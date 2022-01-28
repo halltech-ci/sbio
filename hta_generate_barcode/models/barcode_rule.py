bc36e7a67546dd8bf996920b196dfa2fe6a2340c
ATCHE Maurice <maurice.atche@halltech-africa.com>
2022-01-28 06:17:48 +0000

    Adding hta_lot_barcode

--- hta_generate_barcode/models/barcode_rule.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api, _
-
-
-class BarcodeRule(models.Model):
-    _inherit = "barcode.rule"
-
-    generate_model = fields.Selection(selection_add=[("product.product", "Products")])
