b38aea375eba69365a7d8493ff8f2203b1d31650
ATCHE Maurice <maurice.atche@halltech-africa.com>
2022-01-30 19:51:29 +0000

    Adding conditioning uom in hta_lot_barcode

--- hta_lot_barcode/__manifest__.py
+++ hta_lot_barcode/__manifest__.py
@@ -20,13 +20,15 @@
     'version': '15.0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['mrp', 'stock'],
+    'depends': ['mrp', 'stock', 'barcodes'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        #views
+        'views/product_product_views.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
