69d3a716cab52db20805840095104b49b120c4c9
ATCHE Maurice <maurice.atche@halltech-africa.com>
2022-02-10 18:04:16 +0000

    Adding action_validate in product_concersion

--- hta_product_conversion/models/product_conversion.py
+++ hta_product_conversion/models/product_conversion.py
@@ -16,12 +16,82 @@ class ProductConversion(models.Model):
     src_lot = fields.Many2one('stock.production.lot', string='Source Lot')
     src_product_tracking = fields.Selection(related='src_product_id.tracking', readonly=True)
     from_location = fields.Many2one('stock.location', string='Source Location')
-    qty_to_convert = fields.Float(string="Quantity To Convert")
+    qty_to_convert = fields.Float(string="Quantity To Convert", digits='Product Unit of Measure')
     conversion_line = fields.One2many('product.conversion.line', 'conversion_id', string='Conversion Line')
+    product_ids = fields.Many2many('product.product', string='product ids', 
+                                   compute="_compute_store_convertible_products"
+                                  )
+    user_id = fields.Many2one('res.users', string='User', default=lambda self: self.env.user)
     company_id = fields.Many2one('res.company', 'Company', default=lambda self: self.env['res.company']._company_default_get('product.conversion'))
     date = fields.Date(string='Date', index=True, default=time.strftime('%Y-%m-%d'))
     
     
+    @api.depends('src_product_id')
+    def _compute_store_convertible_products(self):
+        lst = []
+        if self.src_product_id:
+            for prod in self.src_product_id.conversion_line:
+                lst.append(prod.convertible_product.id)
+                self.product_ids = [(6, 0, [i for i in lst])]
+    
+    def validate(self):
+        if not self.conversion_line:
+            raise UserError(_('Veuillez choisir les produits'))
+        inventory_loss = self.env['stock.location'].search([('usage', '=', 'inventory'), ('scrap_location', '=', False), ('return_location', '=', False)], limit=1)
+        if not inventory_loss:
+            raise UserError(_('Kindly map a conversion location'))
+        allocte_qty = 0.0
+        for line in self.conversion_line:
+            if line.allocate_quantity == 0.0:
+                raise UserError(_('Veuillez indiquer la quantité à allouer'))
+            allocte_qty += line.allocate_quantity
+        if allocte_qty != self.qty_to_convert:
+            raise UserError(_('The given allocate Quantity must be equal to the Quantity given for conversion.'))
+        vals_from = {
+            'name': self.name + '/ SL',
+            'product_id': self.src_product_id.id,
+            'location_id': self.from_location.id,
+            'location_dest_id': inventory_loss.id,
+            'product_uom_qty': self.qty_to_convert,
+            'product_uom': self.src_uom.id,
+            'quantity_done': self.qty_to_convert,
+            'state': 'confirmed',
+            'move_line_ids': [(0, 0, {
+                'product_id': self.src_product_id.id,
+                'lot_id': self.src_lot.id or '',
+                'product_uom_qty': 0,
+                'product_uom_id': self.src_uom.id,
+                'qty_done': self.qty_to_convert,
+                'location_id': self.from_location.id,
+                'location_dest_id': inventory_loss.id,
+            })]
+        }
+        stock_move_from = self.env['stock.move'].create(vals_from)
+        stock_move_from._action_done()
+        for line in self.conversion_line:
+            vals_to = {
+                'name': self.name + '/ DL',
+                'product_id': line.dest_product_id.id,
+                'location_id': inventory_loss.id,
+                'location_dest_id': line.to_location.id,
+                'product_uom_qty': line.converted_qty,
+                'product_uom': line.dest_uom.id,
+                'quantity_done': line.converted_qty,
+                'state': 'confirmed',
+                'move_line_ids': [(0, 0, {
+                    'product_id': line.dest_product_id.id,
+                    'lot_id': line.dest_lot.id,
+                    'product_uom_qty': 0,
+                    'product_uom_id': line.dest_uom.id,
+                    'qty_done': line.converted_qty,
+                    'location_id': inventory_loss.id,
+                    'location_dest_id': line.to_location.id,
+                })]
+            }
+            stock_move_to = self.env['stock.move'].create(vals_to)
+            stock_move_to._action_done()
+        self.write({'state': 'done'})
+        return True
     
 class ProductConversionLinem(models.Model):
     _name = "product.conversion.line"
@@ -43,9 +113,9 @@ class ProductProduct(models.Model):
     _inherit = "product.product"
 
     conversion_line = fields.One2many('product.line', 'prod_id', string='Conversion')
-    #duplicate_product_check = fields.Boolean(string='Duplicate Product Check', compute="_compute_check_duplicate_convertible_product")
+    duplicate_product_check = fields.Boolean(string='Duplicate Product Check', compute="_compute_check_duplicate_convertible_product")
     
-    """
+    
     @api.depends('conversion_line.convertible_product')
     def _compute_check_duplicate_convertible_product(self):
         lst = []
@@ -56,7 +126,6 @@ class ProductProduct(models.Model):
             else:
                 raise UserError(_('Conversion Ratio for ' + str(conversion.convertible_product.name) + ' has already been set'))
         self.duplicate_product_check = True
-    """
 
 
 class ProductLine(models.Model):
