bc6646cbfa2bf4762af2ca021a4e77173a9d3be0
ATCHE Maurice <maurice.atche@halltech-africa.com>
2022-02-14 00:49:39 +0000

    Adding hta_custom_product and hta_res_partner

--- hta_res_partner/__manifest__.py
+++ hta_res_partner/__manifest__.py
@@ -27,7 +27,7 @@
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
-        'views/hta_res_partner',
+        'views/hta_res_partner.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
