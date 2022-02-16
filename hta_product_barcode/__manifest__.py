5ae747ed135b1829f286dfee84cd641aafdbe27b
ATCHE Maurice <maurice.atche@halltech-africa.com>
2022-02-16 21:02:46 +0000

    Adding views hta_product_barcode

--- hta_product_barcode/__manifest__.py
+++ hta_product_barcode/__manifest__.py
@@ -24,9 +24,11 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'views/product_template_views.xml',
+        'views/product_product_views.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
