d8b95109f71e9b43b4c893d0ac92f3f1dc375167
sagno <sagnodaouda@gmail.com>
2022-02-21 08:59:06 +0000

    modication hta_pos

--- hta_pos/models/pos_order.py
+++ hta_pos/models/pos_order.py
@@ -12,6 +12,22 @@ class HtaPos(models.Model):
     )
 
     date_delivery = fields.Datetime()
+    customer_Phone = fields.Char()
+    delivery_phone = fields.Char()
+   
+    
+    
+    @api.onchange("partner_id")
+    def _onchange_customer_Phone(self):
+        for rec in self:
+            if rec.partner_id:
+                rec.customer_Phone = rec.partner_id.phone
+                
+    @api.onchange("delivery_person")
+    def _onchange_delivery_phone(self):
+        for rec in self:
+            if rec.delivery_person:
+                rec.delivery_phone = rec.delivery_person.phone
 
 
 # class AssignPos(models.Model):
