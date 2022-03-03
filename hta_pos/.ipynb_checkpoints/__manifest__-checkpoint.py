19b4391794e36243938b7c6149cd15627386827a
Jonas Kacou <jonasgnangbi@gmail.com>
2022-03-03 10:32:07 +0000

    Add licence

--- hta_pos/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ hta_pos/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -16,7 +16,7 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Sales/Point of Sale',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
@@ -49,4 +49,5 @@
     'demo': [
         'demo/demo.xml',
     ],
+    'license': 'LGPL-3',
 }
