f2a12151be3a83a54b9e0b30336c8395398068b2
Jonas Kacou <jonasgnangbi@gmail.com>
2022-03-03 11:47:48 +0000

    delete modules

--- hta_custom_product/models/product_template.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
-
-class ProductTemplate(models.Model):
-    _inherit = "product.template"
-    
-    company_id = fields.Many2one("res.company", default=lambda self: self.env.company)
--- hta_res_partner/models/.ipynb_checkpoints/res_partner-checkpoint.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
-
-
-class ResPartner(models.Model):
-    _inherit = "res.partner"
-    
-    company_id = fields.Many2one("res.company", default=lambda self: self.env.company)
