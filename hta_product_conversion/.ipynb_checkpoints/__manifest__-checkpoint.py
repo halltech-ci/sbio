6cc27bbb2c1a3ac0283ade5adf2ef4f3b4bd32cf
Jonas Kacou <jonasgnangbi@gmail.com>
2022-03-07 17:49:14 +0000

    delete depends hta_product_conversion

--- hta_product_conversion/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ hta_product_conversion/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -20,7 +20,7 @@
     'version': '15.0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'stock', 'product'],
+    'depends': ['base', 'stock', 'mrp'],
 
     # always loaded
     'data': [
@@ -29,6 +29,8 @@
         'views/templates.xml',
         #views
         'views/product_conversion_views.xml',
+        #data
+        'data/product_conversion_sequence.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
