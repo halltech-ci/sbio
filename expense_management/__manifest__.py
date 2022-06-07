be84aa0c7e6acb768fb4694246946bfac3c4e91b
ATCHE Maurice <maurice.atche@halltech-africa.com>
2022-06-07 21:28:49 +0000

    Adding correct currency_field in expense_management

--- expense_management/__manifest__.py
+++ expense_management/__manifest__.py
@@ -17,7 +17,7 @@
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Human Resources/Depenses',
-    'version': '13.0.1',
+    'version': '15.0.1',
 
     # any module necessary for this one to work correctly
     'depends': ['base',
