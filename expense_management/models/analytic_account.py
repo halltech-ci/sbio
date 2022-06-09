39e5ff4c292bcb5b667c6badc695901d2412f77c
ATCHE Maurice <maurice.atche@halltech-africa.com>
2022-06-09 08:10:19 +0000

    Deleting expense_management

--- expense_management/models/analytic_account.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
-
-
-class AccountAnalyticLine(models.Model):
-    _inherit = 'account.analytic.line'
-    
-    expense_line = fields.One2many('expense.line', 'analytic_line')
