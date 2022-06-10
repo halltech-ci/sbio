85c89e251f4ad9770323fbcbc98cf0d70af8bcba
ATCHE Maurice <maurice.atche@halltech-africa.com>
2022-06-10 06:59:36 +0000

    Adding expense_management

--- expense_management/models/expense_request.py
+++ expense_management/models/expense_request.py
@@ -3,7 +3,7 @@
 from odoo import models, fields, api
 
 class ExpenseRequest(models.Model):
-    _name "expense.request"
+    _name = "expense.request"
     _description = "Manage expense request workflow"
     _inherit = ['mail.thread', 'mail.activity.mixin']
     
