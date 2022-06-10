29d0812df058f074a40e6054e69f890af7aeb494
ATCHE Maurice <maurice.atche@halltech-africa.com>
2022-06-10 06:26:25 +0000

    Adding expense_management

---
+++ expense_management/__manifest__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "expense_management",
+
+    'summary': """
+        Workflow for expense management""",
+
+    'description': """
+        Long description of module's purpose
+    """,
+
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Human Resources/Expenses',
+    'version': '15.0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['account', 'web_tour'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+    'license': 'LGPL-3',
+}
