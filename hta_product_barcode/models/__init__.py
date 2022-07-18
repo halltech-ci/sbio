9fe2820e9eff680718d16f536ed72546e805625a
ATCHE Maurice <maurice.atche@halltech-africa.com>
2022-07-18 08:19:04 +0000

    Deactivating product_barcode

--- hta_product_barcode/models/__init__.py
+++ hta_product_barcode/models/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
 from . import models
-from . import product_barcode
-from . import product_product
-from . import product_template
+#from . import product_barcode
+#from . import product_product
+#from . import product_template
