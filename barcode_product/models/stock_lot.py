ebffc80ce2c43fdeb7283fbb24688a2fb1221fa4
jonas kacou <jonasgnangbi@gmail.com>
2022-08-18 18:24:42 +0000

    Report barcode

--- barcode_product/models/stock_lot.py
+++ barcode_product/models/stock_lot.py
@@ -9,15 +9,14 @@ class barcode_product(models.Model):
     
 
     date_create = fields.Datetime(string='Create Date', default=datetime.today())
-    time_expire = fields.Datetime(string='Date Expirate',compute='_compute_exp_date')
+    time_expire = fields.Datetime(string='Date Expirate',default=lambda self: self._compute_exp_date())
     
     @api.depends('date_create')
     def _compute_exp_date(self):
         if self.date_create:
             date_create = self.date_create
             self.time_expire = datetime(date_create.year,date_create.month,date_create.day) + timedelta(730)
-        else:
-            self.time_expire = datetime.today() + timedelta(730)
+
         
     @api.onchange('date_create','product_id.use_expiration_date','product_id.expiration_time')
     def onc_expirate_date_expire(self):
