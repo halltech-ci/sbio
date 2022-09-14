0dc0b40d5e1d584a341443646b87e5db5a18ddc4
Jonas Kacou <jonasgnangbi@gmail.com>
2022-09-14 00:50:15 +0000

    Manifest add depend sale_entreprise

--- Comptabilte/report_account/__manifest__.py
+++ Comptabilte/report_account/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','account_accountant','sale'],
+    'depends': ['base','account_accountant','sale','sale_enterprise'],
 
     # always loaded
     'data': [
