ab3e624a40d47c95466872ef31f51b9f6827c39c
Jonas Kacou <jonasgnangbi@gmail.com>
2022-12-31 10:13:43 +0000

    Report Pos

--- hta_pos/report/.ipynb_checkpoints/report_pivot-checkpoint.py
+++ hta_pos/report/.ipynb_checkpoints/report_pivot-checkpoint.py
@@ -1,20 +1,16 @@
 
-from odoo import models, fields, api
+from odoo import models, fields, api,tools
 
 class HtaReportPos(models.Model):
     _inherit = 'report.pos.order'
-    
-
-    price_paid = fields.Float(string='Montant Payé', readonly=True)
-    
+    # _auto = False
     def _select(self):
         return """
             SELECT
                 MIN(l.id) AS id,
                 COUNT(*) AS nbr_lines,
-                s.date_order AS date,
+                s.create_date AS date,
                 SUM(l.qty) AS product_qty,
-                SUM(s.amount_paid / CASE COALESCE(s.currency_rate, 0) WHEN 0 THEN 1.0 ELSE s.currency_rate END) AS price_paid,
                 SUM(l.qty * l.price_unit / CASE COALESCE(s.currency_rate, 0) WHEN 0 THEN 1.0 ELSE s.currency_rate END) AS price_sub_total,
                 SUM(ROUND((l.qty * l.price_unit) * (100 - l.discount) / 100 / CASE COALESCE(s.currency_rate, 0) WHEN 0 THEN 1.0 ELSE s.currency_rate END, cu.decimal_places)) AS price_total,
                 SUM((l.qty * l.price_unit) * (l.discount / 100) / CASE COALESCE(s.currency_rate, 0) WHEN 0 THEN 1.0 ELSE s.currency_rate END) AS total_discount,
@@ -22,7 +18,7 @@ class HtaReportPos(models.Model):
                     WHEN SUM(l.qty * u.factor) = 0 THEN NULL
                     ELSE (SUM(l.qty*l.price_unit / CASE COALESCE(s.currency_rate, 0) WHEN 0 THEN 1.0 ELSE s.currency_rate END)/SUM(l.qty * u.factor))::decimal
                 END AS average_price,
-                SUM(cast(to_char(date_trunc('day',s.date_order) - date_trunc('day',s.create_date),'DD') AS INT)) AS delay_validation,
+                SUM(cast(to_char(date_trunc('day',s.create_date) - date_trunc('day',s.create_date),'DD') AS INT)) AS delay_validation,
                 s.id as order_id,
                 s.partner_id AS partner_id,
                 s.state AS state,
@@ -39,3 +35,35 @@ class HtaReportPos(models.Model):
                 s.account_move IS NOT NULL AS invoiced,
                 SUM(l.price_subtotal - l.total_cost / CASE COALESCE(s.currency_rate, 0) WHEN 0 THEN 1.0 ELSE s.currency_rate END) AS margin
         """
+
+    
+    def _group_by(self):
+        return """
+            GROUP BY
+                s.id, s.create_date, s.partner_id,s.state, pt.categ_id,
+                s.user_id, s.company_id, s.sale_journal,
+                s.pricelist_id, s.account_move, s.create_date, s.session_id,
+                l.product_id,
+                pt.categ_id, pt.pos_categ_id,
+                p.product_tmpl_id,
+                ps.config_id
+        """
+
+    
+    def update_date_rport(self):
+    	for record in self._context.get('active_ids'):
+            report = self.env[self._context.get('active_model')].browse(record)
+            if report.date:
+                pass
+            else:
+                if report.order_id.date_order:
+                    date_report = report.order_id.date_order
+                    report.date = date_report
+                elif report.order_id.order_date:
+                    date_report = report.order_id.order_date
+                    report.write({'date':date_report})
+                    # report.date = date_report
+                else:
+                    pass
+                
+    
