5ca21cec2f51c9569b6cf5d1e06abba87d5ef83e
Jonas Kacou <jonasgnangbi@gmail.com>
2023-01-09 14:35:37 +0000

    Button livraison

--- hta_pos/models/.ipynb_checkpoints/pos_order-checkpoint.py
+++ hta_pos/models/.ipynb_checkpoints/pos_order-checkpoint.py
@@ -111,15 +111,15 @@ class HtaPos(models.Model):
     	#view_id = self.env.ref('point_of_sale.payment_command_wizard').id
     	for record in self._context.get('active_ids'):
             order = self.env[self._context.get('active_model')].browse(record)
-            if order.state in ['invoiced','done','paid']:
-                for rs in order.lines:
-                    if 'ivraison' in rs.full_product_name:
+            order_lines = order.lines
+            if order.state != 'draft':
+                for rs in order_lines:
+                    if 'ivraison' in str(rs.full_product_name):
                         line = {
                             "price_unit": 0,
                             "price_subtotal": 0,
                             'price_subtotal_incl': 0,
                             }
-
                         rs.write(line)
                     rs._onchange_amount_line_all()
     
