a2135db36aab69bc648e15682bb165cd4ae2bc90
Jonas Kacou <jonasgnangbi@gmail.com>
2023-01-28 15:46:39 +0000

    Wizard Coupon

--- hta_coupon/__manifest__.py
+++ hta_coupon/__manifest__.py
@@ -24,9 +24,10 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'wizard/coupon_wizard_views.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
