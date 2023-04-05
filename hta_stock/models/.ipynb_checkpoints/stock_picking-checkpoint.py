6ec7ff2f4d30aa3711fa7a0c52d5d524eef8c4f6
Ahua Nevrit <nevritahua.na@gmail.com>
2023-04-05 15:03:54 +0000

    Restriction sur les transfert interne

--- hta_stock/models/.ipynb_checkpoints/stock_picking-checkpoint.py
+++ hta_stock/models/.ipynb_checkpoints/stock_picking-checkpoint.py
@@ -1,9 +1,16 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, fields, api, _
+from odoo.exceptions import AccessError
 
 
-class hta_stock(models.Model):
+class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
+
     receipt_user = fields.Many2one('res.users', string="Utlisateur Receip")
+    
+    def button_my_custom_action(self):
+        if self.receipt_user != self.env.user:
+            raise AccessError(_("Vous n'êtes pas autorisé à effectuer cette action."))
+        return self.button_validate()
