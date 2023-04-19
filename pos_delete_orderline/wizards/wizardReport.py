cc9b55d4a35b3939d36d64b2487654cbea1b2c33
Yann-react <blamusjunior@gmail.com>
2023-04-19 15:27:49 +0000

    new update by Yann

--- pos_delete_orderline/wizards/wizardReport.py
+++ pos_delete_orderline/wizards/wizardReport.py
@@ -9,4 +9,19 @@ class CreateWizardReport(models.TransientModel):
     listProduct = fields.Many2many('product.product',string='Liste De Produit', domain="[('sale_ok', '=', True)]")
     
     def action_print_report(self):
-        return self.env.ref('pos_delete_orderline.report_ticket_print').report_action(self)
+        products = self.env['product.product'].browse(self.listProduct.ids)
+        data = {
+            "list_products": products,
+            "form": self.read()[0]
+        }
+        selectproduct = data['form']['listProduct']
+        listproduit = self.env['product.product'].search([('id','in',selectproduct)])
+        liste_de_produit = []
+        for item in listproduit:
+            liste_de_produit.append({
+                'name':item.partner_ref,
+                'price':item.lst_price,
+            })
+        data["listproduit"] = liste_de_produit
+        print("Data ==>>",data)
+        return self.env.ref('pos_delete_orderline.report_ticket_print').report_action(self, data=data)
