7750bdee996f4c0df282bd2ebc627042f2396d46
Jonas Kacou <jonasgnangbi@gmail.com>
2023-05-05 15:01:59 +0000

    RApport par numero de lot

--- CRM/crm_report_sale/__manifest__.py
+++ CRM/crm_report_sale/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['crm','sale','sale_enterprise'],
+    'depends': ['crm','sale','sale_enterprise','point_of_sale'],
 
     # always loaded
     'data': [
