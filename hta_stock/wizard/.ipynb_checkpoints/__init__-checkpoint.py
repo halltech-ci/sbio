62ba3fdc9cc16b5016d00fa56c169e9bdda537ad
Jonas Kacou <jonasgnangbi@gmail.com>
2023-10-30 10:06:53 +0000

    hta_stock

---
+++ hta_stock/wizard/.ipynb_checkpoints/__init__-checkpoint.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from . import assign_commands_wizard
+from . import payment_after_delivery
+from . import wizard_report_pos
+from . import wizard_report_customer_sale
+from . import audit_validate
